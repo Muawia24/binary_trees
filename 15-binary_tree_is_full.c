c190a67d28a646c7370e773c10e49e929398ee08
Muawia24 <amuawia666@gmail.com>
2024-06-06 17:30:18 -0700

    15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -6,14 +6,17 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int l, r;
+
 	if (tree == NULL)
 		return (0);
-	if ((tree->left && tree->right) || (!tree->left && !tree->right))
-		if (tree->parent)
-			return (1);
-
-	binary_tree_is_full(tree->left);
-	binary_tree_is_full(tree->right);
-
+	if ((tree->left == NULL && tree->right == NULL))
+		return (1);
+	if (tree->left && tree->right)
+	{
+		l = binary_tree_is_full(tree->left);
+		r = binary_tree_is_full(tree->right);
+		return (l * r);
+	}
 	return (0);
 }
